8d48c5f935bec479e3ee5f7188115bc9728ae5ef
Lucas Assis <lucasassis0507@gmail.com>
2024-10-21 05:46:14 -0300

    date validity

--- public/frontend/validityDate.js
+++ public/frontend/validityDate.js
@@ -1,8 +1,38 @@
 function validityDate() {
   const getDate = document.querySelector(".date");
-  getDate.addEventListener("input", () => {
-    return console.log(getDate.value);
+  const evento = getDate.addEventListener("input", () => {
+    return dateValidation(new Date(getDate.value));
   });
+}
+function dateValidation(date) {
+  if (date.getDay() === 0) {
+    date = "Segunda";
+    return console.log(date);
+  }
+  if (date.getDay() === 1) {
+    date = "Terça";
+    return console.log(date);
+  }
+  if (date.getDay() === 2) {
+    date = "Quarta";
+    return console.log(date);
+  }
+  if (date.getDay() === 3) {
+    date = "Quinta";
+    return console.log(date);
+  }
+  if (date.getDay() === 4) {
+    date = "Sexta";
+    return console.log(date);
+  }
+  if (date.getDay() === 5) {
+    date = "Sabado";
+    return console.log(date);
+  }
+  if (date.getDay() === 6) {
+    date = "Domingo";
+    return console.log(date);
+  }
 }
 validityDate();
 console.log("teste");
